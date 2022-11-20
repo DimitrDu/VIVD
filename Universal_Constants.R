48c581da7feec962b315ca15031a4e62728caafe
DimitrDu <68843145+DimitrDu@users.noreply.github.com>
2022-11-20 15:42:55 +0100

    Add files via upload

---
+++ Universal_Constants.R
@@ -0,0 +1,13 @@
+# Universal constants ----------------------------------------------------------
+
+UConstants <- list(
+  # Gas constant in Kg*m^2*s^-2*K^-1*mol^-1 (J*K^-1*mol^-1)(Ref 1)
+  R = 8.314,
+  
+  # Internal energy of phase change for air to water partitioning in J*mol^-1 
+  # (Kg*m^2*s*^-2*mol^-1) (Ref 2) 
+  deltaUaw = 60000,
+  
+  # Internal energy of phase change for octanol to water in J*mol^-1 (Ref 2)
+  deltaUow = -20000
+)
