142b1ded39ace3b92b8f465c93a69fe61aa03e37
DimitrDu <68843145+DimitrDu@users.noreply.github.com>
2022-11-20 15:43:59 +0100

    Add files via upload

---
+++ execute_model.R
@@ -0,0 +1,64 @@
+library(data.table)
+library(ggplot2)
+
+source('Code Repo/Universal_Constants.R')
+source('Code Repo/System_Constants.R')
+# source('Code Repo/Compound_Constants.R')
+source('Code Repo/Initializations.R')
+source('Code Repo/Partitioning.R')
+
+cpds <- readxl::read_xlsx('./20221120_Compounds.xlsx')
+
+results <- data.table()
+
+for (i in 1:nrow(cpds)) {
+  CpdConstants <- as.list(cpds[i,])
+  
+  CpdConstants <- init_Compound_Paras(UConstants, SysConstants, CpdConstants)
+  fuFBSdilu <- binding_serum(SysConstants, CpdConstants)
+  KPlastic <- partitioning_plastics(CpdConstants)
+  KAir <- partitioning_air(UConstants, SysConstants, CpdConstants)
+  K <- partitioning_cells(SysConstants, CpdConstants)
+  
+  K[['Air']] <- KAir
+  K[['Plastic']] <- KPlastic
+  
+  C <- MassBalance(SysConstants, CpdConstants, fuFBSdilu, K)
+  M <- perctMass(C, SysConstants)
+  
+  names(C) <- paste0('C_', names(C))
+  names(M) <- paste0('M_', names(M))
+
+  results <- rbind(results, cbind(as.data.table(cpds[i,]), 
+                                  # as.data.table(t(K)), 
+                                  as.data.table(t(C)), 
+                                  as.data.table(t(M))))
+}
+
+writexl::write_xlsx(results, './results.xlsx')
+
+# To plot Cell Concentration with logPow
+
+results <- data.table()
+for (i in seq(-200, 1000, 2)) {
+  CpdConstants$log_Pow <- i/100.0
+  
+  CpdConstants <- init_Compound_Paras(UConstants, SysConstants, CpdConstants)
+  fuFBSdilu <- binding_serum(SysConstants, CpdConstants)
+  KPlastic <- partitioning_plastics(CpdConstants)
+  KAir <- partitioning_air(UConstants, SysConstants, CpdConstants)
+  K <- partitioning_cells(SysConstants, CpdConstants)
+  K[['Air']] <- KAir
+  K[['Plastic']] <- KPlastic
+
+  C <- MassBalance(SysConstants, CpdConstants, fuFBSdilu, K)
+
+  results <- rbind(results, cbind(as.data.table(CpdConstants), as.data.table(t(C))))
+}
+
+show(
+  ggplot(results, aes(log_Pow, CellU)) +
+    geom_point(color = 'red') + geom_hline(yintercept = 1e-9) +
+   #  geom_point(aes(y = Plastic), color = 'blue', size = 1) +
+    scale_y_log10(limits = c(1e-15, 1e-3)) + ggtitle('Cell Concentration vs logPow')
+)
